db31de3d9943a3c680960f5dbc6becc733b26d2d
jash.liao <jash.liao@gmail.com>
2020-08-11 22:09:48 +0800

    Add 202008112209

---
+++ 3組復健 腿部小體操，拉筋、增加肌耐力/123.bat
@@ -0,0 +1,3 @@
+ffmpeg -ss 00:00:47 -t 00:00:24 -i "001.mp4" -vcodec copy -acodec copy  "01.mp4"
+ffmpeg -ss 00:01:13 -t 00:00:22 -i "001.mp4" -vcodec copy -acodec copy  "02.mp4"
+ffmpeg -ss 00:01:38 -t 00:00:30 -i "001.mp4" -vcodec copy -acodec copy  "03.mp4"
